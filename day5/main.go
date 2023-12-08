ecde8c9e506d0f11a913d7a243eb19dfd50f1d25
Landon Beach <lanbeach@amazon.com>
2023-12-08 05:09:08 +0000

    Day 5 part 1 complete

--- day5/main.go
+++ day5/main.go
@@ -2,23 +2,147 @@ package main
 
 import (
 	"AdventOfCode/utils"
-	"bufio"
 	"fmt"
+	"math"
+	"os"
+	"regexp"
+	"strings"
 )
 
-// Thoughts:
-// Create Seed struct containing soil, fertilizer, water, light, temperature, humidity, and location values
-// Create the "maps" for each stage transition. Maybe a struct with methods?
-//   Destination, Source, Range
-//   Delta (dest minus source) to convert from one value to the other value
+type Entry struct {
+	Seed        int
+	Soil        int
+	Fertilizer  int
+	Water       int
+	Light       int
+	Temperature int
+	Humidity    int
+	Location    int
+}
+
+type Mapping struct {
+	Destination int
+	Source      int
+	Range       int
+}
+
+type Almanac struct {
+	Maps map[string][]Mapping
+}
+
+func NewAlmanac() *Almanac {
+	return &Almanac{
+		Maps: make(map[string][]Mapping),
+	}
+}
+
+func (al *Almanac) AddMapping(name string, mapping Mapping) {
+	al.Maps[name] = append(al.Maps[name], mapping)
+}
+
+func (al *Almanac) find(mapName string, from int) int {
+	for _, mapping := range al.Maps[mapName] {
+		if from >= mapping.Source && from < (mapping.Source+mapping.Range) {
+			return mapping.Destination + (from - mapping.Source)
+		}
+	}
+
+	return from
+}
+
+func (al *Almanac) FindSeedEntry(seed int) Entry {
+	entry := Entry{
+		Seed: seed,
+	}
+
+	entry.Soil = al.find("seed-to-soil", entry.Seed)
+	entry.Fertilizer = al.find("soil-to-fertilizer", entry.Soil)
+	entry.Water = al.find("fertilizer-to-water", entry.Fertilizer)
+	entry.Light = al.find("water-to-light", entry.Water)
+	entry.Temperature = al.find("light-to-temperature", entry.Light)
+	entry.Humidity = al.find("temperature-to-humidity", entry.Temperature)
+	entry.Location = al.find("humidity-to-location", entry.Humidity)
+
+	fmt.Println("Entry:", entry)
+
+	return entry
+}
 
 func main() {
-	file := utils.OpenFile("input.txt")
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+	fileImport, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
 	}
+
+	file := strings.Split(string(fileImport), "\n")
+
+	var seedValues []int
+	for _, seed := range strings.Split(file[0], " ")[1:] {
+		seedValues = append(seedValues, utils.StringToInt(seed))
+	}
+
+	almanac := NewAlmanac()
+
+	mapNames := []string{
+		"seed-to-soil",
+		"soil-to-fertilizer",
+		"fertilizer-to-water",
+		"water-to-light",
+		"light-to-temperature",
+		"temperature-to-humidity",
+		"humidity-to-location",
+	}
+
+	// Seed to soil map
+	for _, name := range mapNames {
+		for _, mapping := range extractMap(file, name) {
+			almanac.AddMapping(name, createMapping(mapping))
+		}
+	}
+
+	closestLocation := math.MaxInt
+	for _, seed := range seedValues {
+		entry := almanac.FindSeedEntry(seed)
+		if entry.Location < closestLocation {
+			closestLocation = entry.Location
+		}
+	}
+
+	fmt.Println("Closest Location:", closestLocation)
+}
+
+func createMapping(mapping []int) Mapping {
+	newMapping := Mapping{
+		Destination: mapping[0],
+		Source:      mapping[1],
+		Range:       mapping[2],
+	}
+
+	return newMapping
+}
+
+func extractMap(file []string, mapName string) [][]int {
+	var foundMap bool
+	var mapping [][]int
+	numberPattern := `\b(\d+)\b`
+	re := regexp.MustCompile(numberPattern)
+
+	for _, line := range file {
+		if strings.Contains(line, mapName) {
+			foundMap = true
+			continue
+		}
+
+		if foundMap {
+			if entry := re.FindAllString(line, -1); entry != nil {
+				mapping = append(mapping, utils.StringsToInts(entry))
+			} else {
+				foundMap = false
+				break
+			}
+
+		}
+	}
+
+	return mapping
 }
--- utils/utils.go
+++ utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func OpenFile(path string) *os.File {
@@ -13,4 +14,21 @@ func OpenFile(path string) *os.File {
 	}
 
 	return file
+}
+
+func StringsToInts(line []string) []int {
+	var ints []int
+	for _, element := range line {
+		ints = append(ints, StringToInt(element))
+	}
+	return ints
+}
+
+func StringToInt(str string) int {
+	value, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+
+	return value
 }
